cfd5eebd5ab186103b8ab4b61deba22cee3cb1b2
trumpumpumpum <61837552+trumpumpumpum@users.noreply.github.com>
2020-05-05 01:43:37 +0300

    Add files via upload

--- lesson3_task1.py
+++ lesson3_task1.py
@@ -2,12 +2,10 @@ import random
 N = int(input('Веедите количество значений списка: '))
 x = []
 for i in range(N):
-    x.append(random.randint(0, N))
-    # В random.randint вместо N можно было указать другой диапазон, но решил оставить N
+    x.append(random.randint(0, 1000))
+    # В random.randint можно было указать и другой диапазон, это в задании не обсуждалось
 even_list = [i for i in x if i % 2 == 0]
 if even_list == []:
     print('Четных чисел в списке нет')
 else:
     print(even_list)
-
-
