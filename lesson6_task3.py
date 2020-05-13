90f8b767cb064e5906faa61891d9e3451e6ca7a1
trumpumpumpum <61837552+trumpumpumpum@users.noreply.github.com>
2020-05-13 23:41:48 +0300

    Add files via upload

--- lesson6_task3.py
+++ lesson6_task3.py
@@ -1,7 +1,7 @@
 my_string=input('Введите строку: ').split()
 count=0
 for i in my_string:
-    if len(i)>count:
-        count=len(i)
-        word=i
+     if len(i)>count:
+         count=len(i)
+         word=i
 print('Самое длинное слово: ',word)
