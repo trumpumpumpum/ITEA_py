6113062b8b34048e1c16a52139218f8473f9a3d7
trumpumpumpum <61837552+trumpumpumpum@users.noreply.github.com>
2020-05-15 00:39:32 +0300

    Add files via upload

--- lesson5_task3.py
+++ lesson5_task3.py
@@ -1,8 +1,10 @@
 from random import randint
 import time
+start_time = time.time()
 
 List = []
 for i in range(100000):
     List.append(randint(0, 1000))
-print('Список случайных элементов: \n', List, '\nВремя выполнения: ', time.process_time())
+print('Список случайных элементов: \n', List, '\nВремя выполнения: ', time.time() - start_time)
+
 
